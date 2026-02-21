c969d886cd1d07114b7eba5d485947db7c4d427f
Trent Pierce <Pierce.trent@gmail.com>
2026-02-21 10:19:06 -0600

    Fix frontend API proxying through Next.js route

--- web/next.config.js
+++ web/next.config.js
@@ -30,14 +30,6 @@ const nextConfig = {
           },
         ]
       },
-      async rewrites() {
-        return [
-          {
-            source: "/api/:path*",
-            destination: "https://35.175.242.222.nip.io/:path*",
-          },
-        ]
-      },
     }),
   webpack: (config, { isServer }) => {
     if (!isServer) {
